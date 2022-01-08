5222b7cd277bfeb33b9e5aef24c8cbce167499e3
Ofoe Fiergbor <ofoefiergbor16@gmail.com>
2022-01-08 09:15:21 +0000

    java 11 runtime in app.properties

--- src/main/java/com/davinci/custockspringbootserver/utils/jwt/JWTFilter.java
+++ src/main/java/com/davinci/custockspringbootserver/utils/jwt/JWTFilter.java
@@ -27,9 +27,9 @@ public class JWTFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader(HEADER);
-        if (authHeader != null && !authHeader.isBlank() && authHeader.startsWith("Bearer ")) {
+        if (authHeader != null && !(authHeader == null) && authHeader.startsWith("Bearer ")) {
             String jwt = authHeader.substring(7);
-            if(jwt.isBlank()){
+            if(jwt ==  null){
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JWT Token in Bearer Header");
             } else {
                 try {
